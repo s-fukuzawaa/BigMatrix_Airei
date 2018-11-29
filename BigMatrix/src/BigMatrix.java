8fd06b4f92e4da7c3dc3a9ffa6d7c13d676d2701
bhscs-build <bhs.cs.build@gmail.com>
2018-11-29 14:42:30 -0800

    Populating starter project: Part 2

---
+++ BigMatrix/src/BigMatrix.java
@@ -0,0 +1,64 @@
+import java.util.List;
+
+public class BigMatrix 
+{
+	public BigMatrix()
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public void setValue(int row, int col, int value)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getValue(int row, int col)
+	{
+		throw new UnsupportedOperationException();		
+	}
+	
+	public List<Integer> getNonEmptyRows()
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public List<Integer> getNonEmptyRowsInColumn(int col)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public List<Integer> getNonEmptyCols()
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public List<Integer> getNonEmptyColsInRow(int row)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getRowSum(int row)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getColSum(int col)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getTotalSum()
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public BigMatrix multiplyByConstant(int constant)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public BigMatrix addMatrix(BigMatrix other)
+	{
+		throw new UnsupportedOperationException();
+	}
+}
