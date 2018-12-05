bdb258d4f15ebda56f4d8caecc0b197627d1b048
s-fukuzawaa <s-fukuzawaa@51S50023543.it.bsd405.org>
2018-12-05 08:56:19 -0800

    debugging for testGetvalue. commit to work on desktop.

--- BigMatrix/src/BigMatrix.java
+++ BigMatrix/src/BigMatrix.java
@@ -4,11 +4,15 @@ import java.util.List;
 public class BigMatrix 
 {
 	private HashMap<Integer, HashMap<Integer,Integer>> rowmap;//HashMap<row,HashMap<col,value>>
+	private HashMap<Integer,Integer> rowinsider;
 	private HashMap<Integer, HashMap<Integer,Integer>> colmap;//HashMap<col,HashMap<row,value>>
+	private HashMap<Integer,Integer> colinsider;
 	public BigMatrix()
 	{
 		rowmap= new HashMap<Integer, HashMap<Integer,Integer>>();
+		rowinsider= new HashMap<Integer,Integer>();
 		colmap= new HashMap<Integer, HashMap<Integer,Integer>>();
+		colinsider= new HashMap<Integer,Integer>();
 	}
 	
 	public void setValue(int row, int col, int value)
@@ -17,22 +21,24 @@ public class BigMatrix
 		{
 			return;
 		}
-		HashMap<Integer,Integer> trow= new HashMap<Integer,Integer>();
-		trow.put(col, value);
-		rowmap.put(row, trow);
-		HashMap<Integer,Integer> tcol= new HashMap<Integer,Integer>();
-		tcol.put(row, value);
-		colmap.put(col, tcol);
+		
+		rowinsider.put(col, value);
+		rowmap.put(row, rowinsider);
+		colinsider.put(row, value);
+		colmap.put(col, colinsider);
+		
+		
 	
 	}
 	
 	public int getValue(int row, int col)
 	{
-		if(this.rowmap.isEmpty()||this.rowmap.containsKey(row)==false||this.rowmap.get(row).containsKey(col)==false)
+		if(this.rowmap.isEmpty()||this.colmap.isEmpty()||rowmap.containsKey(row)==false||colmap.containsKey(col)==false)
 		{
 			return 0;
 		}
-		return rowmap.get(row).get(col);		
+		return rowmap.get(row).get(col);
+		
 	}
 	
 	public List<Integer> getNonEmptyRows()
@@ -82,9 +88,24 @@ public class BigMatrix
 	public static void main(String[] args)
 	{
 		BigMatrix test= new BigMatrix();
+		test.setValue(0, 0, 1);
+		test.setValue(1000, 10, 2);
+		test.setValue(10, 1000, 3);
+		//test.setValue(0, 1000, 4);
+		//test.setValue(1000, 0, 5);
+		//test.setValue(0, 10, 6);
+		//test.setValue(10, 0, 7);
+
+
 		System.out.println(test.getValue(0,0));
-		//System.out.println(test.getValue(1000, 10));
-		//System.out.println(test.getValue(2, 5));
+		System.out.println(test.getValue(1000, 10));
+		System.out.println(test.getValue(10,1000));
+		System.out.println(test.getValue(0,1000));
+		//System.out.println(test.getValue(1000,0));
+		//System.out.println(test.getValue(0,10));
+		//System.out.println(test.getValue(10,0));
+
+
 
 		
 	}
